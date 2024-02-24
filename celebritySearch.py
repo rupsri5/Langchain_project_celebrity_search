0ff960491d31e909fa49b139edc5e27645b93d56
Rupali Srivastava <rupalisri52@gmail.com>
2024-02-24 16:14:00 +0530

    added SequentialChain, and ConverationBufferMemory

--- celebritySearch.py
+++ celebritySearch.py
@@ -2,6 +2,8 @@ import os
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 from dotenv import load_dotenv
 
@@ -23,8 +25,39 @@ first_input_prompt=PromptTemplate(
     template="Tell me about {name}"
 )
 
+# memory
+
+person_memory= ConversationBufferMemory(input_key='name', memory_key='chat_history')
+dob_memory=ConversationBufferMemory(input_key='person', memory_key='chat_history')
+desc_memory= ConversationBufferMemory(input_key='dob', memory_key='description_history')
+
 llm=OpenAI(temperature = 0.8)
-chain=LLMChain(llm=llm, prompt=first_input_prompt, verbose=True)
+chain=LLMChain(llm=llm, prompt=first_input_prompt, verbose=True, output_key='person', memory=person_memory)
+
+second_input_prompt=PromptTemplate(
+    input_variables=['person'],
+    template="Mention the year of when was {person} born"
+)
+
+chain2=LLMChain(llm=llm, prompt=second_input_prompt, verbose=True, output_key='dob', memory=dob_memory)
+
+third_input_prompt=PromptTemplate(
+    input_variables=['dob'],
+    template="Mention 5 major points happened in {dob} around the world"
+)
+
+chain3=LLMChain(llm=llm, prompt=third_input_prompt, verbose=True, output_key='description', memory=desc_memory)
+
+parent_chain=SequentialChain(chains=[chain, chain2, chain3], input_variables=['name'], output_variables=['person', 'dob', 'description'], verbose=True)
 
 if input_text:
-    st.write(chain.run(input_text))
+    st.write(parent_chain({'name': input_text}))
+    
+    with st.expander('Person Name'):
+        st.info(person_memory.buffer)
+        
+    with st.expander('Person DOB'):
+        st.info(dob_memory.buffer)
+        
+    with st.expander('description'):
+        st.info(desc_memory.buffer)
